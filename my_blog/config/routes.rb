c08939ce729c2da94a2aa6d5fa1e8157d2c14d3e
NickNish09 <nicholasmarques@cjr.org.br>
2017-06-01 13:35:29 -0300

    Coloquei bootstrap e as paginas

--- my_blog/config/routes.rb
+++ my_blog/config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about',to: 'pages#about'
   get 'segunda_lei',to: 'pages#segunda_lei'
+  get 'mecanica_quantica_livre_arbitrio',to: 'pages#mecanica_quantica_livre_arbitrio'
+  get 'religiao_teoria_do_caos',to: 'pages#religiao_teoria_do_caos'
+  get 'segunda_lei_e_surgimento_da_vida',to: 'pages#segunda_lei_e_surgimento_da_vida'
+  get 'teoria_do_caos',to: 'pages#teoria_do_caos'
+  get 'teoria_do_caos_livre_arbitrio',to: 'pages#teoria_do_caos_livre_arbitrio'
+  get 'teoria_do_vida',to: 'pages#teoria_do_vida'
 end
